51631ba7444a4978caa22ff98ef8306af9f92327
finfantefran <finfantefran@gmail.com>
2020-11-28 20:56:26 +0100

    JS part two

---
+++ LevelOne/JS_PartTwo.js
@@ -0,0 +1,7 @@
+
+// Variable declaration same as java
+var deposit=prompt("How much would you like to deposit?")
+// Pop up message
+alert("You've deposited: "+deposit)
+// console log
+console.log(deposit+" deposited.")
