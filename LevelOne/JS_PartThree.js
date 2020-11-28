975c264c793449d22aaa493ba45fc41d5be97bdc
finfantefran <finfantefran@gmail.com>
2020-11-28 22:22:49 +0100

    PartThree excercise

---
+++ LevelOne/JS_PartThree.js
@@ -0,0 +1,6 @@
+// Input of kg
+var lbs=prompt("What is the weight in pounds (lbs)?")
+// Output alert for de wight conversion
+alert("That is "+lbs*0.453592+"kilograms")
+// Log
+console.log("Coversion Completed")
